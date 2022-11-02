5dcfbe94ef428ef3152165ac8cde65279057a0c6
tajriba <quvvatullayev@gmail.com>
2022-11-02 10:27:13 +0300

    fix template and views

--- projct_app/views.py
+++ projct_app/views.py
@@ -13,15 +13,16 @@ class Hom(View):
 class Dog(View):
     def get(self, request):
         url = 'https://random.dog/woof.json'
-        request = requests.get(url)
-        dog_json = request.json()
+        r = requests.get(url)
+        dog_json = r.json()
         context = dog_json
-        return render(request, 'dog.html', context = context)
+        # return HttpResponse(f'''<img src="{dog_json['url']}"  style="height: 500px">''')
+        return render(request, 'dog.html', context=context)
 
 class Cat(View):
     def get(self, request):
         url = 'https://aws.random.cat/meow'
-        request = requests.get(url)
-        cat_json = request.json()
+        r = requests.get(url)
+        cat_json = r.json()
         context = cat_json
-        return render(request, 'cat.html', context = context)
+        return render(request, 'cat.html', context=context)
